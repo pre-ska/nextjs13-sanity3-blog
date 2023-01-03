a44ff28e8b64bb6e73761d60459c495f07de7fb5
pskaro <Predrag.Skaro@breakwatersolutions.com>
2023-01-03 17:04:41 +0100

    post styling

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import React from 'react';
 import urlFor from '../lib/urlFor';
@@ -11,7 +12,7 @@ const BlogList = ({ posts }: Props) => {
     <div>
       <hr className='border-[#F7AB0A] mb-10' />
 
-      <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-6 pb-24'>
         {posts.map((post) => (
           <div key={post._id} className='group cursor-pointer flex-col flex'>
             <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
@@ -47,8 +48,13 @@ const BlogList = ({ posts }: Props) => {
             </div>
             <div className='mt-5 flex-1'>
               <p className='underline text-lg font-bold'>{post.title}</p>
-              <p className='text-gray-500'>{post.description}</p>
+              <p className='line-clamp-2 text-gray-500'>{post.description}</p>
             </div>
+
+            <p className='flex mt-5 font-bold items-center group-hover:underline'>
+              Read post
+              <ArrowUpRightIcon className='ml-2 h-4 w-4' />
+            </p>
           </div>
         ))}
       </div>
